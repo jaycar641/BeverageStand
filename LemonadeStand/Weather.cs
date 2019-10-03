c2d708cc8101b8fd5b65c89ca626bd853f05585b
Jacob Carson <jaycar641@yahoo.com>
2019-10-03 11:58:08 -0500

    Did constructor in Weather Class

--- LemonadeStand/Weather.cs
+++ LemonadeStand/Weather.cs
@@ -12,8 +12,46 @@ namespace LemonadeStand
         public int temperature;
         private List<string> WeatherConditions = new List<string>();
         public string PredictedForecast; //tells you in the recipe class
+        Random weatherGenerator = new Random();
 
-        Cloudy, Rainy, Sunny, 
+
+        public string weatherConditions 
+        {
+        set
+            {
+                WeatherConditions.Add("Cloudy");
+                WeatherConditions.Add("Rain");
+                WeatherConditions.Add("Sunny");
+                WeatherConditions.Add("Overcast");
+
+            }
+        
+        }
+    
+            public Weather()
+        {
+           
+            int weather = weatherGenerator.Next(0,4);
+
+            switch (weather)
+            {
+                case 0:
+                    condition = "Cloud";
+                    break;
+                case 1:
+                    condition = "Sunny";
+                    break;
+                case 2:
+                    condition = "OverCast";
+                    break;
+                case 3:
+                    condition = "Rain";
+                    break;
+
+            }
+        }
+
+      
     }
 }
 
